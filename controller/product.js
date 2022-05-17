e31f51b640aeb74b7d29fb8ca72c1716af31b526
Jinheng02 <jinheng20.21@ichat.sp.edu.sg>
2022-05-18 00:58:51 +0800

    edited

--- controller/product.js
+++ controller/product.js
@@ -25,7 +25,7 @@ module.exports.createProductTable = function createProductTable(){
 };
 
 module.exports.add = function add(name, price, desc) {
-    return query(`INSERT INTO products (product_name, product_price, product_desc) VALUES($1, $2, $3) RETURNING *`, [name, price, desc])
+    return pool.query(`INSERT INTO products (product_name, product_price, product_desc) VALUES($1, $2, $3) RETURNING *`, [name, price, desc])
         .then(() => console.log("Records Inserted!"))
         .catch((error) => {
             console.log(error);
