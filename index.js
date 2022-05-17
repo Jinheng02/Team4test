b721706dd539a21f28e7d1a6515b44e560a3d051
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-05-17 22:51:25 +0800

    added post message

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const CREATE_TEST_TABLE_SQL = `
     );
 `;
 
+// creating table
 app.post('/test', async (req, res, next) => {
     
     pool.query(CREATE_TEST_TABLE_SQL)
@@ -36,6 +37,31 @@ app.post('/test', async (req, res, next) => {
     .catch((error) => {
         res.send(error);
     });
+});
+
+// insert content into test_table
+app.post('/test/message', async (req, res, next) => {
+    var testm = req.body.testMassage
+    const insertMessage = pool.query(`INSERT INTO test_table (testmessage) VALUES ($1) RETURNING *`, [testm])
+    .then(() => {
+         res.send(insertMessage);
+    })
+    .catch((error) => {
+        res.send(error);
+    });
+});
+
+// get the table
+app.get('/db', async (req, res) => {
+    try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.send(result.result);
+    } catch (err) {
+    console.error(err);
+    res.send(err);
+    }
 });
 
 // make the app listen
