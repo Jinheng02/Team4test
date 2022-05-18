1444eda28930bffcfb726d874c46a98c901aa7eb
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-05-18 12:03:42 +0800

    edited files in MVC format

--- controller/app.js
+++ controller/app.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const cors = require('cors');
 // for user database
+const { createUserTable, addUser } = require("../model/user");
 // for product database
-const { add } = require("../model/product");
-
+const { createProductTable, addProduct } = require("../model/product");
+// to display what port is server running on
 const PORT = process.env.PORT || 3000;
 
 // initialize
@@ -20,15 +21,63 @@ app.get('/',(req, res)=> {
     res.send(`<h1>Server running on port ${PORT}</h1> <h3>Version 1.0</h3>`)
 });
 
+////////////////////////////////////////////////////////
+// THIS SECTION IS USED FOR CREATING TABLES IN DATABASE
+////////////////////////////////////////////////////////
+
+// to create the user table
+app.post('/userTable', async (req, res, next) => {
+    return createUserTable()
+    .then(() => res.status(201).send("User table created!"))
+    .catch(next);
+});
+
+// to create the products table
+app.post('/productTable', async (req, res, next) => {
+    return createProductTable()
+    .then(() => res.status(201).send("Product table created!"))
+    .catch(next);
+});
+
+///////////////////////////////////////////////////
+// END OF SECTION TO CREATE TABLES IN THE DATABASE
+///////////////////////////////////////////////////
+
+
+//////////////////////////////////////////
+// THIS SECTION IS FOR THE USERS DATABASE
+/////////////////////////////////////////
+
+// to add new user to the database
+app.post('/newUser', async (req, res, next) => {
+    const email = req.body.email;
+    const fullname = req.body.fullname;
+
+    return addUser(email, fullname)
+    .then(() => res.status(201).send("New User Inserted Successfully!"))
+    .catch(next);
+});
+/////////////////////////////////////
+// END OF SECTION FOR USERS DATABASE
+/////////////////////////////////////
+
+
+/////////////////////////////////////////////
+// THIS SECTION IS FOR THE PRODUCTS DATABASE
+/////////////////////////////////////////////
+
 // to add new product to the products database
 app.post('/product', async (req, res, next) => {
     const name = req.body.name;
     const price = req.body.price;
     const desc = req.body.desc;
 
-    return add(name, price, desc)
+    return addProduct(name, price, desc)
     .then(() => res.status(201).send("New Records Inserted!"))
     .catch(next);
 });
+////////////////////////////////////////
+// END OF SECTION FOR PRODUCTS DATABASE
+///////////////////////////////////////
 
 module.exports = app;
