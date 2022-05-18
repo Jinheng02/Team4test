64881bec682f967e045b69d4897605314e04f79b
Foo Guo Yi <FGY30.21@ichat.sp.edu.sg>
2022-05-18 11:36:14 +0800

    testing if it works

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // includes
 const app = require('./controller/app');
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-const server = app.listen(PORT, ()=> {
-    console.log(`App listening to port ${PORT}`);
+const server = app.listen(port, ()=> {
+    console.log(`App listening to port ${port}`);
 });
