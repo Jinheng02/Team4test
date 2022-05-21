bae5b810e23a5930e5a3a69e5fb91b6c6ca5d99c
Brennan Sim <hng985@gmail.com>
2022-05-21 17:02:48 +0800

    orderDelete

--- model/order.js
+++ model/order.js
@@ -4,14 +4,15 @@ const CREATE_ORDERS_TABLE = `
     CREATE TABLE orders (
         order_id SERIAL primary key,
         user_id INT NOT NULL,
-        amount REAL NOT NULL,
-        total INT NOT NULL,
-        payment_method VARCHAR(100) NOT NULL,
-        checkout_status VARCHAR(100) NOT NULL,
-        ref VARCHAR(100) NOT NULL,
-        date_created TIMESTAMP without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+        total DECIMAL(8, 2) NOT NULL,
+        date TIMESTAMP without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
     )
 `
+
+const DROP_TABLE_SQL = `
+    DROP TABLE IF EXISTS orders;
+`
+
 // to create orders table 
 module.exports.createOrdersTable = function createOrdersTable() {
     return pool.query(CREATE_ORDERS_TABLE)
@@ -20,7 +21,18 @@ module.exports.createOrdersTable = function createOrdersTable() {
         }).catch((error) => {
             console.log(error);
         })
-}
+};
+
+// to drop/delete orders table 
+module.exports.deleteOrdersTable = function deleteOrdersTable(){
+    return pool.query(DROP_TABLE_SQL)
+        .then(() => {
+            console.log("Orders table deleted!");
+        }).catch((err) => {
+            console.log(err);
+        })
+};
+
 
 module.exports.addOrder = function addOrder(user_id, amount, total, payment_method, checkout_status, ref) {
     return pool.query(`INSERT INTO orders (user_id, amount, total, payment_method, checkout_status, ref) VALUES($1, $2, $3, $4, 'complete', $5) RETURNING *`, [user_id, amount, total, payment_method, checkout_status, ref])
