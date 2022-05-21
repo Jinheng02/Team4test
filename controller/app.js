2db81443cb6c3b3c993da673a2025f81f72b2c6f
amandayong <amandyong@gmail.com>
2022-05-21 17:59:15 +0800

    some conflict

--- controller/app.js
+++ controller/app.js
@@ -27,13 +27,12 @@ const { createCategoryTable,
 
 
 // for cart database
-const { createCartTable } = require('../model/cart');
+const { addCartItem, createCartsTable } = require('../model/cart');
 
 // for orders database
 const { createOrdersTable, addOrder, deleteOrdersTable, alterOrdersTable } = require("../model/order");
-const { addCartItem } = require('../model/cart');
 //const { createProductTable, addProduct } = require("../model/product");
-const { addCartItem, createCartsTable } = require('../model/cart');
+
 
 // to display what port is server running on
 const PORT = process.env.PORT || 3000;
